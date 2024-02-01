ea53792d6cb38ea84385b5f2222b83197be016bc
Chase <chasekat742@gmail.com>
2024-01-31 20:52:16 -0600

    create rapidfire powerup

---
+++ Assets/InheritanceExample/Scripts/Actors/Powerups/RapidFire.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapidFire : PowerUpBase
+{
+	protected override void PowerUp()
+	{
+	}
+
+	protected override void PowerDown()
+	{
+	}
+}
